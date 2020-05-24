81348ab2a6e8ff89d96fa6ff94b81bd90c14f979
yijun-blockin <yijun.wu@block.com>
2020-05-24 14:43:29 +0800

    添加pm2启动项

---
+++ blog.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const chalk = require('chalk')
+const blog = express();
+blog.use(express.static(path.resolve(__dirname, './dist')))
+
+blog.get('*', function(req, res) {
+    const html = fs.readFileSync(path.resolve(__dirname, './dist/index.html'), 'utf-8')
+    res.send(html)
+})
+blog.listen(8080, res => {
+    console.log(chalk.yellow('Start Service On 8080'));
+});
